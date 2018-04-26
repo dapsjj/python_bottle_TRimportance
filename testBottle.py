b2010a713d627351c9076094c17bf5a21d578405
Peter Song <dapsjj@qq.com>
2018-04-26 10:56:36 +0900

    Update testBottle.py

--- testBottle.py
+++ testBottle.py
@@ -7,7 +7,7 @@ from scipy import optimize as opt
 import os
 
 
-def generate(code, year,week):
+def generate(code, year, week):
     kion = pd.read_csv(r'D:/a.csv')
     kion.head()
     Px = np.arange(0, len(kion), 1)
@@ -18,17 +18,17 @@ def generate(code, year,week):
     b = res[0][1]
     c = res[0][2]
     d = res[0][3]
-    print("a = %s" % a)
-    print("b = %s" % b)
-    print("c = %s" % c)
-    print("d = %s" % d)
+    # print("a = %s" % a)
+    # print("b = %s" % b)
+    # print("c = %s" % c)
+    # print("d = %s" % d)
     Px2 = []
     for x in Px:
         Px2.append(a * x ** 3 + b * x ** 2 + c * x + d)
     plt.plot(Px, Py)
     plt.plot(Px, np.array(Px2))
     plt.savefig('./image/test.jpg')
-    bottle.redirect('/show'+'test')
+    # bottle.redirect('/show'+'test')
 
 
 def fit_func(x, a, b, c, d):
@@ -58,17 +58,27 @@ def server_static(filename):
 def server_static(filename):
     return bottle.static_file(filename, root='./fonts')
 
+@bottle.route('/image/<filename>')
+def server_static(filename):
+    return bottle.static_file(filename, root='./image')
+
 # @bottle.route('/locales/<filename>')
 # def server_static(filename):
 #     return bottle.static_file(filename, root='./locales')
 
-@bottle.route('/generate', method='POST')
+@bottle.route('/validate', method='POST')
 def get_para():
-    enployeeCode = bottle.request.POST.get('employeeCode')
+    employeeCode = bottle.request.POST.get('employeeCode')
     reportYear = bottle.request.POST.get('reportYear')
     reportWeek = bottle.request.POST.get('reportWeek')
-    if enployeeCode and reportYear and reportWeek:
-        generate(enployeeCode, reportYear,reportWeek)
+    print("employeeCode:"+str(employeeCode))
+    print("reportYear:" +str(reportYear))
+    print("reportWeek:" + str(reportWeek))
+    if employeeCode and reportYear and reportWeek:
+        generate(employeeCode, reportYear,reportWeek)
+        return "sucess"
+    else:
+        return "fail"
 
 @bottle.error(404)
 def error404(error):
